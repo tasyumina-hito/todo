abee78a45f7dc2b7ba25e5288fc1e4501393f7b2
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 17:20:24 +0900

    中断(エラー:10行目)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
+use thiserror::Error;
+
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Faild to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+
+impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
